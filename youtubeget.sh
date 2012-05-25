a6418a865d56a039640dd60def3676e060ae0d67
Dainel Vera <dainel@kurubit.com>
2012-05-25 09:35:50 -0400

    original idea

--- youtubeget.sh
+++ youtubeget.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
+# Simply call ./youtubeget.sh VIDEO URL
+# Requirements youtube-dl
+
+# For Ubuntu 
+#	sudo apt-get install youtube-dl
+
+# For Mac 
+#	sudo port install youtube-dl
+
+# Debugger
+#set -x 
 
 videourl=$1
+
 
 # Check if Video URL is true
 if [[ ${videourl}zzzzz == zzzzz ]]
@@ -8,18 +20,18 @@ then
     echo "Video URL Is Missing"
     exit 1
 fi
-
-# If Video URl is true then download
-	cd Video
 		
+		# Bootstrap title into a variable
 		title="$(youtube-dl -e $1)"
 		
-		youtube-dl $1 -o "$title.mp4" &&
-		
-		rm "$title.mp4" &&
-		
-		cd ..
-	
+		# Start downlading the video
+		youtube-dl $1 -o "Videos/$title.mp4" 
+						
+		# Start the convertion to mp3
+		ffmpeg -i "download.mp4" -acodec libmp3lame -ac 2 -ab 128k -vn -y "test.mp3" 
+				
+		#rm "Video/$title.mp4" 
+			
 
 # Close it up
 exit
